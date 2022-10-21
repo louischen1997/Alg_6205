57d08e810be1d9207815705e4a16b268a29c6a59
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:27:44 -0700

    cmmt

--- digitalmarketing/Supplier/Supplier.java
+++ digitalmarketing/Supplier/Supplier.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.Supplier;
 
 import java.util.ArrayList;
@@ -11,10 +7,7 @@ import digitalmarketing.ProductManagement.ProductCatalog;
 import digitalmarketing.ProductManagement.ProductSummary;
 import digitalmarketing.ProductManagement.ProductsReport;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class Supplier {
 
     SupplierDirectory directory;
