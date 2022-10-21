ec2caf352908cc2c1a08932519c660e9710d80b6
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:22:17 -0700

    cmmt

--- digitalmarketing/OrderManagement/Order.java
+++ digitalmarketing/OrderManagement/Order.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.OrderManagement;
 
 import java.util.ArrayList;
@@ -10,10 +6,7 @@ import digitalmarketing.CustomerManagement.CustomerProfile;
 import digitalmarketing.Personnel.EmployeeProfile;
 import digitalmarketing.ProductManagement.Product;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class Order {
 
     ArrayList<OrderItem> orderitems;
