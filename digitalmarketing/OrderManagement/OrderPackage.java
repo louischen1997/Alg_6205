d1bd2c2e10a062865f011cd4c6a010b03d09619a
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:23:03 -0700

    cmmt

--- digitalmarketing/OrderManagement/OrderPackage.java
+++ digitalmarketing/OrderManagement/OrderPackage.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.OrderManagement;
 import digitalmarketing.ProductManagement.Product;
 import java.util.ArrayList;
-/**
- *
- * @author kevinchen
- */
+
 public class OrderPackage {
     String name;
     String survice;
