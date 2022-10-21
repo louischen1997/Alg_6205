ae0fcbd25dd800a30d93454e542be33493daef92
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:09:20 -0700

    cmmt

--- Hop.java
+++ Hop.java
@@ -4,38 +4,24 @@
  * 
  * 
  * To Compile: IntUtil.java RandomInt.java Hop.java HopBase.java
- * 
- * @author Jagadeesh Vasudevamurthy
- * @year 2019
  */
 
 class Hop extends HopBase{
-	//You cannot have any functions or data here
+	
 	Hop() {
-		//NOTHING CAN BE CHANGED HERE
+		
 		testBed();
 	}
 	
 	@Override
   int hopSmart(int [] s, int x) {
-		//NOTHING CAN BE CHANGED HERE
+		
     return alg(s,x) ;
 	}
 
-	/*
-	 * WRITE CODE IN alg
-	 *  YOU CANNOT USE ANY static variable in this function
-	 *  YOU can use many local variables inside the function
-	 *  Cannot use any loop statements like:  for, while, do, while, go to
-	 *  Can use if
-	 *  ONLY AFTER THE execution of this routine array s MUST have the same contents as you got it.
-	 *  YOU cannot call any subroutine inside this function except alg itself 
-	 *  
-	 */
 
 	private int alg(int [] s, int x) {
-		//WRITE CODE
-		//If your code is more than 10 lines, you don't understand the problem
+		
 
 		if(s[x]==x){return 0;}
 		else if(s[x]==-1){return -1;}
