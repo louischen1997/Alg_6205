553bf00d0ed1c80f2310d4ccf54503daaab03a86
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:27:23 -0700

    cmmt

--- digitalmarketing/ProductManagement/SolutionOffer.java
+++ digitalmarketing/ProductManagement/SolutionOffer.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.ProductManagement;
 
 import java.util.ArrayList;
 import digitalmarketing.MarketModel.Market;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class SolutionOffer {
     ArrayList<Product> products;
     int price;
