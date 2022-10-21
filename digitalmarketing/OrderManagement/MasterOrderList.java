c0041c79028682f9651e29ed8971cd2dba411fea
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:21:47 -0700

    cmmt

--- digitalmarketing/OrderManagement/MasterOrderList.java
+++ digitalmarketing/OrderManagement/MasterOrderList.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.OrderManagement;
 
 import java.util.ArrayList;
 import digitalmarketing.Business.Business;
 import digitalmarketing.CustomerManagement.CustomerProfile;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class MasterOrderList {
     Business business;
     ArrayList<Order> orders;
