da5c304a6dce597488aed57905b07a69f34f4af8
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:18:52 -0700

    cmmt

--- digitalmarketing/CustomerManagement/CustomerComparator.java
+++ digitalmarketing/CustomerManagement/CustomerComparator.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package digitalmarketing.CustomerManagement;
 
 import java.util.Comparator;
 
-/**
- *
- * @author alelashvili
- */
+
 public class CustomerComparator implements Comparator<CustomerProfile> {
 
     @Override
