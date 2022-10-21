f1bbc7e31069b86991f617bdb2cd4b0f02adf35c
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:24:30 -0700

    cmmt

--- digitalmarketing/Personnel/EmployeeProfile.java
+++ digitalmarketing/Personnel/EmployeeProfile.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.Personnel;
 
 import java.util.ArrayList;
 import digitalmarketing.OrderManagement.Order;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class EmployeeProfile {
     ArrayList<Order> salesorders;
 
