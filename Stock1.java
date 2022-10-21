cf43da85ebba6cb1d0236a9037482424b85e7917
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:13:58 -0700

    cmmt

--- Stock1.java
+++ Stock1.java
@@ -1,51 +1,38 @@
 /**
  * File Name: Stock1.java 
  * Stock1 concrete class
- * 
- * 
- * To Compile: IntUtil.java RandomInt.java Stock1.java Stock1Base.java
- * 
- * @author Jagadeesh Vasudevamurthy
- * @year 2019
  */
 
 class Stock1 extends Stock1Base{
-	//You can have any number of private data members here
-	//You can have any number of private functions here
+	
 	Stock1() {
-		//NOTHING CAN BE CHANGED HERE
 		testBed() ;
 	}
 	
 	@Override
 	void NSquareTimeConstantSpace() {
-		//NOTHING CAN BE CHANGED HERE
+		
     	nsquareTimeConstantSpace() ;
 	}
 	
 	@Override
   void NlognTimeLognSpace() {
-		//NOTHING CAN BE CHANGED HERE
+		
     	nlognTimelognSpace() ;
 	}
 	
 	@Override
   void NTimeLognSpace() {
-		//NOTHING CAN BE CHANGED HERE
+		
     	nTimelognSpace() ;
 	}
 	void NTimeConstantSpace() {
-		//NOTHING CAN BE CHANGED HERE
+		
 		nTimeConstantSpace() ;
 	}
 
 	
-	/*
-	 * Time: O(n^2)
-	 * Space: THETA(1)
-	 * All your routine should match this answer
-	 * Nothing can be changed in this routine
-	 */
+	
 	private void nsquareTimeConstantSpace() {
 		int gp = 0 ;
 		int l = size() ;
@@ -68,8 +55,7 @@ class Stock1 extends Stock1Base{
 	 */
 
 	private void nlognTimelognSpace() {
-		//must write the routine. UNTIL YOU WRITE UNCOMMENT BELOW
-    	//nsquareTimeConstantSpace();
+		
 		int l = size() ;
 		returnmax(0,l-1);
 	}
@@ -181,8 +167,7 @@ class Stock1 extends Stock1Base{
 	 */
 
 	private void nTimelognSpace() {
-		//must write the routine. UNTIL YOU WRITE UNCOMMENT BELOW
-	    //nsquareTimeConstantSpace();
+		
 		int l = size() ;
 		returnmaxarray(0,l-1);
 	}
@@ -316,13 +301,13 @@ class Stock1 extends Stock1Base{
 
 
     private void nTimeConstantSpace() {
-		//IF YOU CANNOT WRITE USE THE ROUTINE BELOW
+		
 	    nsquareTimeConstantSpace();
     }
 
 
 	public static void main(String[] args) {
-		//NOTHING CAN BE CHANGED HERE
+		
 		System.out.println("Stock1 problem STARTS");
 		Stock1 m = new Stock1() ;
 		System.out.println("All Stock1 tests passed. Now you can pass interviews");
