6132c5a9801a6a149270323b866810b277df877c
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:19:40 -0700

    cmmt

--- digitalmarketing/CustomerManagement/CustomerProfile.java
+++ digitalmarketing/CustomerManagement/CustomerProfile.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.CustomerManagement;
 
 import digitalmarketing.Personnel.Person;
 import java.util.ArrayList;
 import digitalmarketing.OrderManagement.Order;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class CustomerProfile {
     ArrayList<Order> orders;
     Person person;
