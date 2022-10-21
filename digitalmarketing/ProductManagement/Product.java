f5dd9d0d561e03219851a5440ab9bdf093eead9e
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:25:35 -0700

    cmmt

--- digitalmarketing/ProductManagement/Product.java
+++ digitalmarketing/ProductManagement/Product.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.ProductManagement;
 
 import java.util.ArrayList;
 import digitalmarketing.OrderManagement.OrderItem;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class Product {
 
     private String name;
