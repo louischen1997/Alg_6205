dde9c71387d6020b99f4da942c589eefe4409716
Kaiwen Chen <47822448+louischen1997@users.noreply.github.com>
2022-10-21 06:26:01 -0700

    cmmt

--- digitalmarketing/ProductManagement/ProductCatalog.java
+++ digitalmarketing/ProductManagement/ProductCatalog.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalmarketing.ProductManagement;
 
 import java.util.ArrayList;
 import digitalmarketing.Supplier.Supplier;
 import java.util.Collections;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class ProductCatalog {
 
     Supplier supplier;
